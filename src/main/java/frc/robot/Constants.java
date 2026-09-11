eabd2d2f4e4dd01b44785eab81f628676ccaac82
Hayden <142731527+ChiMukwa07@users.noreply.github.com>
2026-09-11 11:54:43 -0400

    Tuned Pathplanner PID
    Tuned Pathplanner PID (IN SIM!!!), needs to be tested

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -27,10 +27,10 @@ public final class Constants {
   }
 
   public static class SwerveConstants {
-      public static final double ROBOT_MASS = (50); 
+      public static final double ROBOT_MASS = Units.kilogramsToLbs(28.085); 
       public static final Matter CHASSIS    = new Matter(new Translation3d(0, 0, Units.inchesToMeters(8)), ROBOT_MASS);
       public static final double LOOP_TIME  = 0.13; //s, 20ms + 110ms sprk max velocity lag
-      public static final double MAX_SPEED  = Units.metersToFeet(5.85216);
+      public static final double MAX_SPEED  = Units.metersToFeet(5.6);
       
   }
   public static final class LightsConstants {
